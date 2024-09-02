22e53f68c4bf6761f4e724824a6d0df838bf1a4a
jcobcorpuz <cc62200@mail.harpercollege.edu>
2024-09-01 23:31:20 -0500

    created method containing bounds
    asks user to input number within a specified bounds

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,27 @@
 package com.csc;
 
-// Your code goes here!
+import java.util.Scanner;
+
+public class Menu {
+    public static int promptInt(String promptMessage, String errorMessage, Integer lowerBound, Integer upperBound){
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+
+        while(true){
+            System.out.println(promptMessage);
+            userInput = scanner.nextInt();
+
+            if((lowerBound == null || userInput >= lowerBound) && (upperBound == null || userInput <= upperBound)){
+                return userInput;
+            }
+            else{
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int result = promptInt("Please enter a number between 20 and 140:","You number is invalid, Please re-enter another number", 20, 140);
+        System.out.println("The number you inputted is " + result + ".");
+    }
+}
